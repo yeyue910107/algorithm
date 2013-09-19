018d973fbef7287cf6a4976e5379a92409c8f59e
Yue Ye <yeyue910107@gmail.com>
2013-09-19 15:11:23 +0800

    Create UniquePermutations.cc

---
+++ leetcode/UniquePermutations.cc
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int>> res;
+        doPermute(num, 0, res);
+        return res;
+    }
+private:
+    void doPermute(vector<int> &num, const int k, vector<vector<int>> &res) {
+        int size = num.size();
+        if (k == size - 1) {
+            res.push_back(num);
+            return ;
+        }
+        
+        for (int i = k; i < size; i++) {
+            if (isSwap(num, k, i)) {
+                swap(num, k, i);
+                doPermute(num, k + 1, res);
+                swap(num, k, i);
+            }
+        }
+    }
+    
+    void swap(vector<int> &num, const int i, const int j) {
+        const int tmp = num[i];
+        num[i] = num[j];
+        num[j] = tmp;
+    }
+
+	bool isSwap(vector<int> &num, const int m, const int n) {
+		for (int i = m; i < n; i++)
+			if (num[i] == num[n])
+				return false;
+		return true;
+	}
+};
