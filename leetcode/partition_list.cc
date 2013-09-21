b28f32f247ec8a26041d4c0601b0759abe3da16d
Yue Ye <yeyue910107@gmail.com>
2013-09-21 16:33:43 +0800

    Update partition_list.cc

--- leetcode/partition_list.cc
+++ leetcode/partition_list.cc
@@ -11,53 +11,53 @@ struct ListNode {
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
-		if (head == NULL)
-			return NULL;
+	if (head == NULL)
+	    return NULL;
         ListNode *p = new ListNode(-1), *q = new ListNode(-1);
         ListNode *pp = p, *qq = q;
         ListNode *r = head;
         while (r != NULL) {
             if (r->val < x) {
-				pp->next = r;
-				pp = r;
+		pp->next = r;
+		pp = r;
             }
             else {
                 qq->next = r;
-				qq = r;
+		qq = r;
             }
-			r = r->next;
+	    r = r->next;
         }
-		pp->next = q->next;
-		qq->next = NULL;
-		ListNode *tmp = p->next;
-		delete p;
-		delete q;
-		return tmp;
+	pp->next = q->next;
+	qq->next = NULL;
+	ListNode *tmp = p->next;
+	delete p;
+	delete q;
+	return tmp;
     }
 };
 
 int main() {
-	int a[6] = {1, 4, 3, 2, 5, 2};
-	ListNode *head = NULL, *p;
-	for (int i = 0; i < 6; i++) {
-		ListNode *q = new ListNode(a[i]);
-		if (head == NULL) {
-			head = q;
-			p = q;
-		}
-		else {
-			p->next = q;
-			p = q;
-		}
+    int a[6] = {1, 4, 3, 2, 5, 2};
+    ListNode *head = NULL, *p;
+    for (int i = 0; i < 6; i++) {
+	ListNode *q = new ListNode(a[i]);
+	if (head == NULL) {
+ 	    head = q;
+	    p = q;
 	}
-	Solution s;
-	head = s.partition(head, 3);
+	else {
+	    p->next = q;
+	    p = q;
+	}
+    }
+    Solution s;
+    head = s.partition(head, 3);
 
+    p = head;
+    while (p != NULL) {
+	head = p->next;
+	delete p;
 	p = head;
-	while (p != NULL) {
-		head = p->next;
-		delete p;
-		p = head;
-	}
-	return 0;
+    }
+    return 0;
 }
