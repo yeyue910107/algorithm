eb2450642494989049dc4f859785c10ffb523539
Yue Ye <yeyue910107@gmail.com>
2013-09-21 14:06:08 +0800

    Update three_sum.cc

--- leetcode/three_sum.cc
+++ leetcode/three_sum.cc
@@ -9,21 +9,21 @@ class Solution {
 public:
     vector<vector<int> > threeSum(vector<int> &num) {
         vector<vector<int>> res;
-		hash_map<int, int> _map;
-		hash_map<int, int>::iterator it;
+	hash_map<int, int> _map;
+	hash_map<int, int>::iterator it;
         int size = num.size();
         if (size < 3)
             return res;
         sort(num.begin(), num.end());
-		for (int i = 0; i < size; i++) {
-			if (_map.find(num[i]) == _map.end())
-				_map.insert(pair<int, int>(num[i], 1));
-			else
-				_map[num[i]]++;
-		}
+	for (int i = 0; i < size; i++) {
+	    if (_map.find(num[i]) == _map.end())
+		_map.insert(pair<int, int>(num[i], 1));
+	    else
+		_map[num[i]]++;
+	    }
         for (int i = 0; i < size; i++) {
-			if (i > 0 && num[i] == num[i - 1])
-				continue;
+	    if (i > 0 && num[i] == num[i - 1])
+		continue;
             int left = -num[i];
             /*int j = i + 1, k = size - 1;
             while (j < k) {
@@ -41,20 +41,20 @@ public:
                 else
                     j++;
             }*/
-			for (int j = i + 1; j < size; j++) {
-				if (j > i + 1 && num[j] == num[j - 1])
-					continue;
-				it = _map.find(left - num[j]);
-				if (it != _map.end() && (it->first > num[j] || (it->first == num[j] && it->second > 1))) {
-					if (it->first == 0 && it->second < 3)
-						continue;
-					vector<int> tmp;
-					tmp.push_back(num[i]);
-					tmp.push_back(num[j]);
-					tmp.push_back(left - num[j]);
-					res.push_back(tmp);
-				}
-			}
+	    for (int j = i + 1; j < size; j++) {
+		if (j > i + 1 && num[j] == num[j - 1])
+		    continue;
+		it = _map.find(left - num[j]);
+		if (it != _map.end() && (it->first > num[j] || (it->first == num[j] && it->second > 1))) {
+		    if (it->first == 0 && it->second < 3)
+			continue;
+	  	    vector<int> tmp;
+		    tmp.push_back(num[i]);
+		    tmp.push_back(num[j]);
+		    tmp.push_back(left - num[j]);
+		    res.push_back(tmp);
+		}
+	    }
         }
         return res;
     }
@@ -65,7 +65,7 @@ int main() {
 	vector<int> num;
 	vector<vector<int>> res;
 	for (int i = 0; i < 4; i++)
-		num.push_back(a[i]);
+	    num.push_back(a[i]);
 	Solution s;
 	res = s.threeSum(num);
 	return 0;
