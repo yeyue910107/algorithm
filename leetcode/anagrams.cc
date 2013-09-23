11d640d29fcd3eb610b4936f5aaeac9de20ea162
Yue Ye <yeyue910107@gmail.com>
2013-09-23 20:02:52 +0800

    Update anagrams.cc

--- leetcode/anagrams.cc
+++ leetcode/anagrams.cc
@@ -3,12 +3,13 @@
 #include <string>
 #include <memory>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
-int cmp(const void *a, const void *b) {
+/*int cmp(const void *a, const void *b) {
     return *(char *)a - *(char *)b;
-}
+}*/
 
 class Solution {
 public:
@@ -16,37 +17,38 @@ public:
         int size = strs.size();
         vector<string> res;
         string tmp;
-        map<string, string> dic;
-        map<string, string>::iterator it;
+        map<string, int> dic;
+        map<string, int>::iterator it;
         for (int i = 0; i < size; i++) {
-            tmp = strSort(strs[i]);
+            tmp = strs[i];
+	    sort(tmp.begin(), tmp.end());
             it = dic.find(tmp);
             if (it == dic.end())
-                dic.insert(pair<string, string>(tmp, strs[i]));
+                dic.insert(pair<string, int>(tmp, i));
             else {
-                if (it->second != "SB") {
-                    res.push_back(it->second);
-                    dic[tmp] = "SB";
+                if (it->second != -1) {
+                    res.push_back(strs[it->second]);
+                    dic[tmp] = -1;
                 }
                 res.push_back(strs[i]);
             }
         }
         return res;
     }
-private:
+/*private:
     char *strSort(const string s) {
         int len = s.size();
         char *res = new char[len+1];
 		strcpy_s(res, len+1, s.c_str());
         qsort(res, len, sizeof(*res), cmp);
         return res;
-    }
+    }*/
 };
 
 int main() {
 	string s[5] = {"", "", "def", "efd", "bn"};
 	vector<string> str, res;
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 5; i++)
 		str.push_back(s[i]);
 	Solution so;
 	res = so.anagrams(str);
