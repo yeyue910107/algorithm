c9e06e0ed4c2f445242a0aa608b65ab2d9480314
Yue Ye <yeyue910107@gmail.com>
2013-10-06 00:12:29 +0800

    Update word_break.cc

--- leetcode/word_break.cc
+++ leetcode/word_break.cc
@@ -10,8 +10,8 @@ bool dp[MAX][MAX];
 public:
     bool wordBreak(string s, set<string> &dict) {
         int size = s.size(), i, j, k;
-		if (size == 0) return true;
-		if (dict.size() == 0) return false;
+	if (size == 0) return true;
+	if (dict.size() == 0) return false;
         memset(dp, false, sizeof(dp));
         for (i = 0; i < size; i++) {
             for (j = 0; j+i < size; j++) {
@@ -32,13 +32,13 @@ public:
 };
 
 int main() {
-	string s = "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb";
-	string sdic[50] = {"abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"};
-	set<string> dict;
-	for (int i = 0; i < 50; i++) {
-		dict.insert(sdic[i]);
-	}
-	Solution so;
-	bool res = so.wordBreak(s, dict);
-	return 0;
+    string s = "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb";
+    string sdic[50] = {"abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"};
+    set<string> dict;
+    for (int i = 0; i < 50; i++) {
+        dict.insert(sdic[i]);
+    }
+    Solution so;
+    bool res = so.wordBreak(s, dict);
+    return 0;
 }
