459d45e600363b0d1d3493e37470a6b6df0e25de
Yue Ye <yeyue910107@gmail.com>
2013-10-06 11:55:04 +0800

    Update set_matrix_zeroes.cc

--- leetcode/set_matrix_zeroes.cc
+++ leetcode/set_matrix_zeroes.cc
@@ -6,7 +6,7 @@ using namespace std;
 class Solution {
 public:
     void setZeroes(vector<vector<int> > &matrix) {
-        int m = matrix.size();
+		int m = matrix.size();
 		if (m == 0) return;
 		int n = matrix[0].size(), mm, nn, i, j;
 		if (n == 0) return;
