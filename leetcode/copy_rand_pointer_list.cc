5e9297ca497c13aaa8e0e5ecc2ce0e6748ca077b
Yue Ye <yeyue910107@gmail.com>
2013-10-10 11:40:34 +0800

    Update copy_rand_pointer_list.cc

--- leetcode/copy_rand_pointer_list.cc
+++ leetcode/copy_rand_pointer_list.cc
@@ -54,31 +54,79 @@ public:
             }
             p = p->next;
         }
-		return newhead;
+        return newhead;
+    }
+
+    RandomListNode *copyRandomList2(RandomListNode *head) {
+        copyList(head);
+        setRandom(head);
+        return detachList(head);
+    }
+
+private:
+    void copyList(RandomListNode *&head) {
+        if (head == NULL)
+            return ;
+        RandomListNode *p = head, *tmp = NULL;
+        while (p != NULL) {
+            RandomListNode *q = new RandomListNode(p->label);
+            tmp = p->next;
+            p->next = q;
+            q->next = tmp;
+            p = tmp;
+        }
+    }
+
+    void setRandom(RandomListNode *&head) {
+        RandomListNode *p = head;
+        while (p != NULL) {
+            if (p->random == NULL)
+                p->next->random = NULL;
+            else
+                p->next->random = p->random->next;
+            p = p->next->next;
+        }
+    }
+
+    RandomListNode *detachList(RandomListNode *head) {
+        if (head == NULL) return NULL;
+        RandomListNode *p = head, *q = NULL, *res = head->next;
+        
+        while (p != NULL) {
+            q = p->next;
+            p->next = q->next;
+            p = p->next;
+            if (p != NULL)
+                q->next = p->next;
+            else
+                q->next = NULL;
+        }
+        return res;
     }
 };
 
 int main() {
-	RandomListNode *head = NULL, *p;
-	RandomListNode *rdlist[MAX];
-	srand((unsigned)time(0));
-	for (int i = 0; i < MAX; i++) {
-		RandomListNode *r = new RandomListNode(i);
-		rdlist[i] = r;
-	}
-	for (int i = 0; i < MAX; i++) {
-		if (head == NULL) {
-			head = rdlist[0];
-			p = head;
-		}
-		else {
-			p->next = rdlist[i];
-			p = rdlist[i];
-		}
-		p->random = rdlist[rand() % MAX];
-	}
-	RandomListNode *newhead;
-	Solution s;
-	newhead = s.copyRandomList(head);
-	return 0;
+    RandomListNode *head = NULL, *p;
+    RandomListNode *rdlist[MAX];
+    srand((unsigned)time(0));
+    for (int i = 0; i < MAX; i++) {
+        RandomListNode *r = new RandomListNode(i);
+        rdlist[i] = r;
+    }
+    for (int i = 0; i < MAX; i++) {
+        if (head == NULL) {
+            head = rdlist[0];
+            p = head;
+        }
+        else {
+            p->next = rdlist[i];
+            p = rdlist[i];
+        }
+        p->random = rdlist[rand() % MAX];
+    }
+    RandomListNode *newhead;
+    Solution s;
+    //newhead = s.copyRandomList(head);
+    newhead = s.copyRandomList2(head);
+    return 0;
 }
